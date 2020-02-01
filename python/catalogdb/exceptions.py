85d1f3d75db970d99fd09635923fb9c15397e0c0
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-31 19:35:48 -0800

    Additional changes

--- python/catalogdb/exceptions.py
+++ python/catalogdb/exceptions.py
@@ -2,13 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under a 3-clause BSD license.
-#
-# @Author: Brian Cherinka
-# @Date:   2017-12-05 12:01:21
-# @Last modified by:   Brian Cherinka
-# @Last Modified time: 2017-12-05 12:19:32
-
-from __future__ import print_function, division, absolute_import
 
 
 class CatalogdbError(Exception):
@@ -33,23 +26,6 @@ class CatalogdbNotImplemented(CatalogdbError):
         super(CatalogdbNotImplemented, self).__init__(message)
 
 
-class CatalogdbAPIError(CatalogdbError):
-    """A custom exception for API errors"""
-
-    def __init__(self, message=None):
-        if not message:
-            message = 'Error with Http Response from Catalogdb API'
-        else:
-            message = 'Http response error from Catalogdb API. {0}'.format(message)
-
-        super(CatalogdbAPIError, self).__init__(message)
-
-
-class CatalogdbApiAuthError(CatalogdbAPIError):
-    """A custom exception for API authentication errors"""
-    pass
-
-
 class CatalogdbMissingDependency(CatalogdbError):
     """A custom exception for missing dependencies."""
     pass
@@ -61,11 +37,6 @@ class CatalogdbWarning(Warning):
 
 class CatalogdbUserWarning(UserWarning, CatalogdbWarning):
     """The primary warning class."""
-    pass
-
-
-class CatalogdbSkippedTestWarning(CatalogdbUserWarning):
-    """A warning for when a test is skipped."""
     pass
 
 
